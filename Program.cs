93174932bc559a53a8bd2d2d4f48a5519be5a1f1
regino-pan-a-i <reginopanai@gmail.com>
2022-09-20 16:11:58 -0600

    first backup

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
+
+
+// See https://aka.ms/new-console-template for more information
